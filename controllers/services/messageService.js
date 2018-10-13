1620abd3de3fd3c002c96763c424d1265f6e7f04
mstar95 <m.star@wp.pl>
2018-10-13 19:59:35 +0200

    trying2

--- controllers/services/messageService.js
+++ controllers/services/messageService.js
@@ -15,8 +15,8 @@ const sendMessage = (senderId, message) => {
 
 exports.sendUrlMessage = (senderId, text, url) => {
     const struct = urlStruct(text, url)
-    console.log(struct.toJson())
-    sendMessage(senderId, struct.toJson())
+    console.log(struct.toString())
+    sendMessage(senderId, struct.toString())
 };
 
 exports.sendTextMessage = (senderId, text) => {
